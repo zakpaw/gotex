bbfa254c609afc5a2030c61f96cebf9aa4051d72
zakpaw <zakpaw36@gmail.com>
2023-12-20 18:17:26 +0100

    refactor: components -> component

--- pkg/handler/view.go
+++ pkg/handler/view.go
@@ -1,13 +1,13 @@
 package handler
 
 import (
-    "github.com/zakpaw/gotex/pkg/components"
+	"github.com/zakpaw/gotex/pkg/view"
 
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/adaptor"
-    "github.com/a-h/templ"
+	"github.com/a-h/templ"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
 
 func IndexPage(c *fiber.Ctx) error {
-    return adaptor.HTTPHandler(templ.Handler(components.IndexPage()))(c)
+    return adaptor.HTTPHandler(templ.Handler(view.IndexPage()))(c)
 }
