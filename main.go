08d92a729a96d89b1cb471b19472762645658c78
zakpaw <zakpaw36@gmail.com>
2023-12-20 18:15:37 +0100

    feat: add logging middleware

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+    "github.com/zakpaw/gotex/pkg/router"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/zakpaw/gotex/pkg/router"
+    "github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+    app.Use(logger.New())
 
 	router.SetupRoutes(app)
 
